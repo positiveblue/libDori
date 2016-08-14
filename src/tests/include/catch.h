c05ddc726b197bf8f524265d6f01496921b2d634
Jordi Montes Sanabria <jomsdev@gmail.com>
2016-08-13 23:54:02 -0700

    catch.h: Import catch_lib.h fixed.

--- src/tests/include/catch.h
+++ src/tests/include/catch.h
@@ -6,6 +6,6 @@
 #define LIBSTREAM_CATCH_H
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "catch_lib.h"
 
 #endif //LIBSTREAM_CATCH_H
