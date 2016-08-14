2a114f5e5384b68239a86ae830e3f7b9b96a965f
Jordi Montes Sanabria <jomsdev@gmail.com>
2016-08-13 23:35:36 -0700

    WIP Tests: Configuring the test library.
    Catch: TODO rm from the header file.

---
+++ src/tests/tests_main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jordi on 8/13/2016.
+//
+
+#include "catch.h"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
