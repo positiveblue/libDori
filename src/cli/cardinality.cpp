301992a58a55dbdf38daa5be372eb5fd3dc71c58
Jordi Montes <jomsdev@gmail.com>
2016-09-02 22:45:02 -0700

    Cardinality: cli now is using Dummy & LogLog algorithms.

--- src/cli/cardinality.cpp
+++ src/cli/cardinality.cpp
@@ -5,14 +5,19 @@
 
 int main() {
     ICardinality<std::string> *counter_ptr = new DummyCounter<std::string>();
-
+    ICardinality<std::string> *LogLog_ptr = new LogLog<std::string>(7);
     std::string s;
-    while(std::cin >> s)
+
+    while(std::cin >> s) {
         counter_ptr->offer(s);
+        LogLog_ptr->offer(s);
+    }
 
-    std::cout << "All the stream procesed..." << std::endl;
 
-    std::cout << counter_ptr->cardinality() << ' ' << counter_ptr->elementsOffered() << std::endl;
+    std::cout << "DummyCounter: " << counter_ptr->cardinality() << ' ' << counter_ptr->elementsOffered() \
+        << ' ' << counter_ptr->cardinality()/counter_ptr->cardinality() << std::endl;
+    std::cout << "LogLog: " << LogLog_ptr->cardinality() << ' ' << LogLog_ptr->elementsOffered() \
+        << ' ' << counter_ptr->cardinality()/(double)LogLog_ptr->cardinality() << std::endl;
 
     return 0;
 }
