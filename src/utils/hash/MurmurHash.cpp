140160f636642e61c5bdc3658a4f755d5113919f
jms <jomsdev@gmail.com>
2016-12-11 17:40:34 -0800

    Murmurhash.cpp style fixed

--- src/utils/hash/MurmurHash.cpp
+++ src/utils/hash/MurmurHash.cpp
@@ -24,95 +24,95 @@
 
 namespace ls { namespace utils {
 
-MurmurHash::MurmurHash() {
-  // TODO(Jordi): Check if the random generator is well done for 32 and 64!
-  unsigned int mt19937Seed =
-    std::chrono::system_clock::now().time_since_epoch().count();
+  MurmurHash::MurmurHash() {
+    // TODO(Jordi): Check if the random generator is well done for 32 and 64!
+    unsigned int mt19937Seed =
+      std::chrono::system_clock::now().time_since_epoch().count();
 
-  // mt19937 is a standard mersenne_twister_engine
-  std::mt19937 generator (mt19937Seed);
+    // mt19937 is a standard mersenne_twister_engine
+    std::mt19937 generator (mt19937Seed);
 
-  seed32 = generator();
-  seed64 = generator();
-}
-
-MurmurHash::MurmurHash(std::uint32_t _seed) : seed32 (_seed) {}
-
-MurmurHash::MurmurHash(std::uint64_t _seed) : seed64 (_seed) {}
-
-std::uint32_t MurmurHash::hash(const char *ptr, std::uint32_t size) {
-  std::uint32_t c1 = 0xcc9e2d51;
-  std::uint32_t c2 = 0x1b873593;
-  std::uint32_t r1 = 15;
-  std::uint32_t r2 = 13;
-  std::uint32_t m = 5;
-  std::uint32_t n = 0xe6546b64;
-  std::uint32_t h = 0;
-  std::uint32_t k = 0;
-  std::uint8_t *d = (uint8_t *) ptr; // 32 bit extract from `key'
-  const std::uint32_t *chunks = NULL;
-  const std::uint8_t *tail = NULL; // tail - last 8 bytes
-  int i = 0;
-  int l = size / 4; // chunk length
-
-  h = seed32;
-
-  chunks = (const std::uint32_t *) (d + l * 4); // body
-  tail = (const std::uint8_t *) (d + l * 4); // last 8 byte chunk of `key'
-
-  // for each 4 byte chunk of `key'
-  for (i = -l; i != 0; ++i) {
-    // next 4 byte chunk of `key'
-    k = chunks[i];
-
-    // encode next 4 byte chunk of `key'
-    k *= c1;
-    k = (k << r1) | (k >> (32 - r1));
-    k *= c2;
-
-    // append to hash
-    h ^= k;
-    h = (h << r2) | (h >> (32 - r2));
-    h = h * m + n;
+    seed32 = generator();
+    seed64 = generator();
   }
 
-  k = 0;
+  MurmurHash::MurmurHash(std::uint32_t _seed) : seed32 (_seed) {}
 
-  // remainder
-  switch (size & 3) { // `len % 4'
-    case 3: k ^= (tail[2] << 16);
-    case 2: k ^= (tail[1] << 8);
+  MurmurHash::MurmurHash(std::uint64_t _seed) : seed64 (_seed) {}
 
-    case 1:
-      k ^= tail[0];
+  std::uint32_t MurmurHash::hash(const char *ptr, std::uint32_t size) {
+    std::uint32_t c1 = 0xcc9e2d51;
+    std::uint32_t c2 = 0x1b873593;
+    std::uint32_t r1 = 15;
+    std::uint32_t r2 = 13;
+    std::uint32_t m = 5;
+    std::uint32_t n = 0xe6546b64;
+    std::uint32_t h = 0;
+    std::uint32_t k = 0;
+    std::uint8_t *d = (uint8_t *) ptr; // 32 bit extract from `key'
+    const std::uint32_t *chunks = NULL;
+    const std::uint8_t *tail = NULL; // tail - last 8 bytes
+    int i = 0;
+    int l = size / 4; // chunk length
+
+    h = seed32;
+
+    chunks = (const std::uint32_t *) (d + l * 4); // body
+    tail = (const std::uint8_t *) (d + l * 4); // last 8 byte chunk of `key'
+
+    // for each 4 byte chunk of `key'
+    for (i = -l; i != 0; ++i) {
+      // next 4 byte chunk of `key'
+      k = chunks[i];
+
+      // encode next 4 byte chunk of `key'
       k *= c1;
       k = (k << r1) | (k >> (32 - r1));
       k *= c2;
+
+      // append to hash
       h ^= k;
+      h = (h << r2) | (h >> (32 - r2));
+      h = h * m + n;
+    }
+
+    k = 0;
+
+    // remainder
+    switch (size & 3) { // `len % 4'
+      case 3: k ^= (tail[2] << 16);
+      case 2: k ^= (tail[1] << 8);
+
+      case 1:
+        k ^= tail[0];
+        k *= c1;
+        k = (k << r1) | (k >> (32 - r1));
+        k *= c2;
+        h ^= k;
+    }
+
+    h ^= size;
+
+    h ^= (h >> 16);
+    h *= 0x85ebca6b;
+    h ^= (h >> 13);
+    h *= 0xc2b2ae35;
+    h ^= (h >> 16);
+
+    return h;
   }
 
-  h ^= size;
+  std::uint32_t MurmurHash::hash(const std::string &str) {
+    return hash(str.data(), str.size());
+  }
 
-  h ^= (h >> 16);
-  h *= 0x85ebca6b;
-  h ^= (h >> 13);
-  h *= 0xc2b2ae35;
-  h ^= (h >> 16);
+  std::uint64_t MurmurHash::hash64(const char *ptr, std::uint32_t size) {
+    return hash(ptr, size);
+  }
 
-  return h;
-}
-
-std::uint32_t MurmurHash::hash(const std::string &str) {
-  return hash(str.data(), str.size());
-}
-
-std::uint64_t MurmurHash::hash64(const char *ptr, std::uint32_t size) {
-  return hash(ptr, size);
-}
-
-std::uint64_t MurmurHash::hash64(const std::string &str) {
-  return hash(str.data(), str.size());
-}
+  std::uint64_t MurmurHash::hash64(const std::string &str) {
+    return hash(str.data(), str.size());
+  }
 
 }  // namespace utils
 }  // namespace ls
