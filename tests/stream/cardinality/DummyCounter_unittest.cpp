0484d5dd4f9007aff45964f06dedf45870286700
jms <jomsdev@gmail.com>
2016-12-11 17:41:48 -0800

    DummyCounter unittests added

---
+++ tests/stream/cardinality/DummyCounter_unittest.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include "catch/catch_lib.hpp"
+#include "stream/cardinality/DummyCounter.hpp"
+
+TEST_CASE( "Creating DummyCounter object", "[DummyCounter]" ) {
+  ls::stream::ICardinality* DC = new ls::stream::DummyCounter();
+}
+
+// Testing with 3 datasets provide with the library
+TEST_CASE( "DummyCounter cardinality for D1.txt", "[DummyCounter]" ) {
+  // Data file path
+  std::string data_path = "../datasets/D1.txt";
+  ls::stream::ICardinality* DC = new ls::stream::DummyCounter();
+
+  std::ifstream file(data_path);
+
+  if (!file) {
+    REQUIRE( false );
+  }
+
+  std::string line;
+  while (!file.eof()) {
+    std::getline(file, line);
+    DC->offer(line);
+  }
+
+  REQUIRE(DC->cardinality() == 3187);
+  REQUIRE(DC->elementsOffered() == 17221);
+  
+  file.close();
+}
+
+TEST_CASE( "DummyCounter cardinality for D2.txt", "[DummyCounter]" ) {
+  // Data file path
+  std::string data_path = "../datasets/D2.txt";
+  ls::stream::ICardinality* DC = new ls::stream::DummyCounter();
+
+  std::ifstream file(data_path);
+
+  if (!file) {
+    REQUIRE( false );
+  }
+
+  std::string line;
+  while (!file.eof()) {
+    std::getline(file, line);
+    DC->offer(line);
+  }
+
+  REQUIRE(DC->cardinality() == 23136);
+  REQUIRE(DC->elementsOffered() == 384224);
+  
+  file.close();
+}
+
+TEST_CASE( "DummyCounter cardinality for D3.txt", "[DummyCounter]" ) {
+  // Data file path
+  std::string data_path = "../datasets/D3.txt";
+  ls::stream::ICardinality* DC = new ls::stream::DummyCounter();
+
+  std::ifstream file(data_path);
+
+  if (!file) {
+    REQUIRE( false );
+  }
+
+  std::string line;
+  while (!file.eof()) {
+    std::getline(file, line);
+    DC->offer(line);
+  }
+
+  REQUIRE(DC->cardinality() == 5895);
+  REQUIRE(DC->elementsOffered() == 61884);
+  
+  file.close();
+}
