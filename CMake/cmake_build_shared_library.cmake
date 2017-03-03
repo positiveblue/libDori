1a14a803162ccb11d62b6cbe8a898c84eff930aa
Jordi Montes <jomsdev@gmail.com>
2017-03-03 13:49:26 -0800

    CMAKE_MACOSX_RPATH for CMake 2.8 added

--- CMake/cmake_build_shared_library.cmake
+++ CMake/cmake_build_shared_library.cmake
@@ -3,7 +3,7 @@
 option (BUILD_SHARED_LIBS "Whether we should build it as a shared library or not" ON)
 if(BUILD_SHARED_LIBS)
   set(LIBRARY_TYPE SHARED)
-
+  set(CMAKE_MACOSX_RPATH 1)
   # Define the shared library for Dori
   # -------------------------------------
   add_library(Dori ${LDORI_C_CPP_SOURCE} ${LDORI_THIRD_PARTY})
@@ -11,7 +11,7 @@ if(BUILD_SHARED_LIBS)
   # Compiler flags
   # -------------------------------------
   if(CMAKE_CXX_FLAGS)
-    set_target_properties(Dori PROPERTIES LINK_FLAGS ${CMAKE_CXX_FLAGS})
+    set_target_properties(Dori PROPERTIES COMPILE_FLAGS ${CMAKE_CXX_FLAGS})
   endif()
 
   install (TARGETS Dori DESTINATION lib/)
