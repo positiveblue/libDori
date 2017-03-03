38fd31e7564e316c81d02b3c86bcbe75a20406eb
Jordi Montes <jomsdev@gmail.com>
2017-03-03 14:01:10 -0800

    Testing comeback to cmake 3.0 on Travis CI

--- CMake/cmake_build_shared_library.cmake
+++ CMake/cmake_build_shared_library.cmake
@@ -3,7 +3,7 @@
 option (BUILD_SHARED_LIBS "Whether we should build it as a shared library or not" ON)
 if(BUILD_SHARED_LIBS)
   set(LIBRARY_TYPE SHARED)
-  set(CMAKE_MACOSX_RPATH 1)
+
   # Define the shared library for Dori
   # -------------------------------------
   add_library(Dori ${LDORI_C_CPP_SOURCE} ${LDORI_THIRD_PARTY})
