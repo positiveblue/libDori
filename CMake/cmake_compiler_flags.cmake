915fff38d8c7c12f16cf3726376fd562a35cb3f4
Jordi Montes <jomsdev@gmail.com>
2017-03-03 10:35:00 -0800

    "-std=c++14" flag added to all compilers

--- CMake/cmake_compiler_flags.cmake
+++ CMake/cmake_compiler_flags.cmake
@@ -1,22 +1,22 @@
 message(STATUS "Building with ${CMAKE_CXX_COMPILER_ID} compiler")
 
+set (CXX14_COMPILER_FLAGS "-std=c++14")
+
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 
-    set (CXX14_COMPILER_FLAGS "-std=c++14")
     set (COMPILER_SPEC_FLAGS
         "-W -Wall -Wno-write-strings -Wno-unused-variable -Wno-unused-parameter -Wno-sign-compare -Wno-unused-local-typedef"
     )
 
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
 
-    set (CXX14_COMPILER_FLAGS "-std=c++14")
+    
     set (COMPILER_SPEC_FLAGS
-            "-W -Wall -Wno-write-strings -Wno-unused-variable -Wno-unused-parameter -Wno-sign-compare -Wno-unused-local-typedef"
+        "-W -Wall -Wno-write-strings -Wno-unused-variable -Wno-unused-parameter -Wno-sign-compare -Wno-unused-local-typedef"
     )
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 
-    set (CXX14_COMPILER_FLAGS "-std=c++14")
     set (COMPILER_SPEC_FLAGS
         "-W -Wall -Wno-write-strings -Wno-strict-aliasing -Wno-format -Wno-deprecated -Wno-unused-variable -Wno-unused-parameter -Wno-sign-compare -Wno-unused-local-typedefs"
     )
