40a230c6052c26fdbeaae1686dc865417cbd3c5b
Jordi Montes <jomsdev@gmail.com>
2017-04-05 15:47:48 -0700

    Removing all the source files

--- tests/stream/cardinality/RegisterSet_unittest.cpp
+++
@@ -1,6 +0,0 @@
-#include "catch/catch_lib.hpp"
-#include "stream/cardinality/RegisterSet.hpp"
-
-TEST_CASE( "Creating RegisterSet object", "[RegisterSet]" ) {
-  dori::stream::RegisterSet RS = dori::stream::RegisterSet(64);
-}
--- tests/utils/hash/MurmurHash_unittest.cpp
+++
@@ -1,6 +0,0 @@
-#include "catch/catch_lib.hpp"
-#include "utils/hash/hash.hpp"
-
-TEST_CASE( "Creating Murmurhash object", "[Murmurhash3]" ) {
-  dori::utils::IHasher* hasher = new dori::utils::MurmurHash();
-}
